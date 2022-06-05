055e192997c69643978eff65a20152670571268b
lollolzz <lollolzz1018@gmail.com>
2022-06-05 21:03:09 +0900

    RePython

---
+++ Python&/String/2.py
@@ -0,0 +1,5 @@
+print('Hello ' + 'World')
+print('Hello ' * 3)
+print('Hello'[0])
+print('Hello'[1])
+print('Hello'[2])
---
+++ Python&/String/4.py
@@ -0,0 +1,6 @@
+print("egoing's \"tutorial\"")
+print("\\")
+print("Hello\nworld")
+print("Hello\tworld")
+print("\a")
+print("Hello\nWorld")
