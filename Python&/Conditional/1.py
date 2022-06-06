3f2624520601f9ca0b83cf64937e2ea4edf8bbd5
lollolzz <lollolzz1018@gmail.com>
2022-06-06 21:57:25 +0900

    RePython

---
+++ Python&/Conditional/1.py
@@ -0,0 +1,21 @@
+if True:
+    print("code1")
+    print("code2")
+print("code3")
+
+# login 성공 했을 경우의 코드
+# input1= 11
+# real1 = 11
+# if real1 == input1:
+#     print('Hello!')
+
+# login 실패했을 경우 +
+input = 11
+real = 11
+if real == input:
+    print('Hello!')
+else:
+    print('Who are you?')
+
+if real != input:
+    print("Who are you?")
