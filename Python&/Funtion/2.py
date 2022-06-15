b42443a48e9af5410fe975b821a2b2e6bba4eaec
lollolzz <lollolzz1018@gmail.com>
2022-06-15 22:36:54 +0900

    RePython

---
+++ Python&/Funtion/2.py
@@ -0,0 +1,11 @@
+def a3():
+    print('before')
+    return 'aaa'
+    print('after')
+# return 뒤에 있는 aaa라는 값이 함수의 값이 되게 된다.
+# return을 해주면 return값을 a3()에 돌려주게 된다.
+# return값을 함수로 보내주고 그 후 함수를 종료 한다.
+print(a3())
+
+
+
